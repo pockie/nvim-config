cd166f54706cd2587540633db8a877b04d3f7c4c
Michael Pokladek <m.pokladek@hotmail.de>
2026-05-24 23:23:36 +0200

    Add which-key

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
-require('plugins.mason')
-require('plugins.nvim-treesitter')
-require('plugins.telescope')
-require('plugins.tokyonight')
+require("plugins.mason")
+require("plugins.nvim-treesitter")
+require("plugins.telescope")
+require("plugins.which-key")
+require("plugins.tokyonight")
 
-require('config.autocmd')
-require('config.options')
-require('config.globals')
-require('config.lsp')
-require('config.keymap')
+require("config.autocmd")
+require("config.options")
+require("config.globals")
+require("config.lsp")
+require("config.keymap")
 
 -- vim.pack.update();
